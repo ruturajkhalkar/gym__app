e48a96a40760267ddec4c9f237dbbecda6729422
ruturaj <ruturajkhalkar@gmail.com>
2022-12-01 12:52:22 +0530

    final5

--- gymapp/src/components/HeroBanner.js
+++ gymapp/src/components/HeroBanner.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import{Box,Stack,Typography} from '@mui/material';
+
 
 export default function HeroBanner() {
   return (
-    <div>
-      
-    </div>
+   
+   <Box sx={{mt:{lg:'212px',xs:'70px'},
+             ml:{sm:'50px'}}}
+             position='realtive'p='20px'>
+    <Typography color='#FF2625' fontWait='600' fontSize='26px'>
+        Fitness club
+    </Typography>
+    <Typography>
+        Sweat,Smile<br/>and Repeat
+    </Typography>
+    <Typography>
+        Check out the most effective exercises
+    </Typography>
+   </Box>
   )
 }
