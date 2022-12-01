bbf5e582e4ca3e91f07fefe58c82bb5924788385
ruturaj <ruturajkhalkar@gmail.com>
2022-12-01 09:44:17 +0530

    final2

--- gymapp/src/App.js
+++ gymapp/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import {Route,Routes} from 'react-router-dom'
+import {Box} from '@mui/material'
+import './App.css';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+ 
 
 export default function App() {
   return (
-    <div>
-      <h1>hello hello</h1>
-    </div>
+    <>
+   <Box width='400px'>
+    <Navbar/>
+    <Routes>
+        <Route path='/' eLement={<Home/>}/>
+        <Route path='/exercise/:id' eLement={<ExerciseDetail/>}/>
+    </Routes>
+    <Footer/>
+   </Box>
+   </>
   )
 }
---
+++ gymapp/src/pages/ExerciseDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ExerciseDetail() {
+  return (
+    <div>
+      <h1>Exercisedetail</h1>
+    </div>
+  )
+}
