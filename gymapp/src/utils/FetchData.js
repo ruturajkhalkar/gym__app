095a4b790d5b6e0ff9a10dd0ab0bed7ec5c44c3e
ruturaj <ruturajkhalkar@gmail.com>
2022-12-06 21:22:28 +0530

    complete

--- gymapp/src/utils/FetchData.js
+++ gymapp/src/utils/FetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOption ={
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': '5799748b04msh61c614149f236eap1e00e2jsn1074881f0305',
+      'X-RapidAPI-Key': '5f0aae35dbmshe4af75c7726d1e0p115f0bjsn15b9bcf81b5a',
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
   };
