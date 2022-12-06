cd2833e2495c61908cdfad00e676ed90a474a734
ruturaj <ruturajkhalkar@gmail.com>
2022-12-06 18:32:54 +0530

    done

--- gymapp/src/components/Footer.js
+++ gymapp/src/components/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import{Box,Stack,Typography} from '@mui/material'
+import Logo from '../assets/images/Logo-1.png'
 export default function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <Box mt='80' bgcolor='#fff3f4'>
+      <Stack gap='40px' alignItems='center' px='40' pt='24px' marginBottom='50px'>
+          <img src={Logo} alt='logo' width='200px' height='40px'/>
+      </Stack>
+      
+    </Box>
   )
 }
